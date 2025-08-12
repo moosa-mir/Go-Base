7fede0191f2fd06cae01eb18a207826e14b9b2c5
moosa <moosa.mir@sternx.de>
2025-08-12 15:29:38 +0330

    amend

--- cmd/serve.go
+++ cmd/serve.go
@@ -21,7 +21,13 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Starts the HTTP server",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := registerRoute.RegisterRoutes()
+		database, err := db.ConnectDB()
+		if err != nil {
+			log.Fatalf("Failed to connect to the database: %v", err)
+		}
+		defer database.Close()
+
+		err = registerRoute.RegisterRoutes(database, database, database, database, database, database, database, database)
 		if err != nil {
 			log.Fatalf("Failed to connect to the database: %v", err)
 		}
--- internal/route/router.go
+++ internal/route/router.go
@@ -18,51 +18,53 @@ import (
 )
 
 // RegisterRoutes registers the login route with the HTTP server
-func RegisterRoutes() error {
-	database, err := db.ConnectDB()
-	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-		return err
-	}
-	defer database.Close()
+func RegisterRoutes(
+	userDB db.UserDBInterface,
+	adminDB db.AdminDBInterface,
+	basketDB db.BasketDBInterface,
+	orderDB db.OrderDBInterface,
+	paymentDB db.PaymentDBInterface,
+	productDB db.ProductDBInterface,
+	sellerDB db.SellerDBInterface,
+	walletDB db.WalletDBInterface) error {
 
 	fmt.Println("Registering routes...")
 	router := mux.NewRouter()
-	accountDB := account.NewAccount(database)
-	productDB := product.NewProduct(database)
-	basketDB := basket.NewBasket(database)
-	walletDB := wallet.ApiHandler(database)
-	paymentDB := payment.NewPayment(database)
-	adminDB := admin.NewAdmin(database)
-	sellerDB := seller.NewSeller(database)
-	orderDB := order.NewOrder(database)
+	newAccount := account.NewAccount(userDB)
+	newProduct := product.NewProduct(productDB)
+	newBasket := basket.NewBasket(basketDB)
+	newWallet := wallet.ApiHandler(walletDB)
+	newPayment := payment.NewPayment(paymentDB)
+	newAdmin := admin.NewAdmin(adminDB)
+	newSeller := seller.NewSeller(sellerDB)
+	newOrder := order.NewOrder(orderDB)
 
 	// mux.HandleFunc("/login", login.LoginHandler)
-	router.HandleFunc("/login", accountDB.LoginHandler).Methods(http.MethodPost)
-	router.HandleFunc("/register", accountDB.RegisterHandler).Methods(http.MethodPost)
-	router.HandleFunc("/product", productDB.ProductListHandler).Methods(http.MethodGet)
-	router.HandleFunc("/product/{id}", productDB.ProductItemHandler).Methods(http.MethodGet)
+	router.HandleFunc("/login", newAccount.LoginHandler).Methods(http.MethodPost)
+	router.HandleFunc("/register", newAccount.RegisterHandler).Methods(http.MethodPost)
+	router.HandleFunc("/product", newProduct.ProductListHandler).Methods(http.MethodGet)
+	router.HandleFunc("/product/{id}", newProduct.ProductItemHandler).Methods(http.MethodGet)
 
-	router.HandleFunc("/seller/register", sellerDB.RegisterHandler).Methods(http.MethodPost)
+	router.HandleFunc("/seller/register", newSeller.RegisterHandler).Methods(http.MethodPost)
 
-	router.HandleFunc("/admin/login", adminDB.AdminLoginHandler).Methods(http.MethodPost)
+	router.HandleFunc("/admin/login", newAdmin.AdminLoginHandler).Methods(http.MethodPost)
 
-	router.HandleFunc("/basket", auth.Middleware(basketDB.WalletItemsHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/basket", auth.Middleware(basketDB.AddItemHandler)).Methods(http.MethodPost)
-	router.HandleFunc("/basket", auth.Middleware(basketDB.DeleteItemHandle)).Methods(http.MethodDelete)
+	router.HandleFunc("/basket", auth.Middleware(newBasket.WalletItemsHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/basket", auth.Middleware(newBasket.AddItemHandler)).Methods(http.MethodPost)
+	router.HandleFunc("/basket", auth.Middleware(newBasket.DeleteItemHandle)).Methods(http.MethodDelete)
 
-	router.HandleFunc("/wallet", auth.Middleware(walletDB.HandleIncreaseWallet)).Methods(http.MethodPost)
-	router.HandleFunc("/wallet", auth.Middleware(walletDB.WalletHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/wallet", auth.Middleware(newWallet.HandleIncreaseWallet)).Methods(http.MethodPost)
+	router.HandleFunc("/wallet", auth.Middleware(newWallet.WalletHandler)).Methods(http.MethodGet)
 
-	router.HandleFunc("/account", auth.Middleware(accountDB.AccountInfoHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/account", auth.Middleware(accountDB.UpdateHandler)).Methods(http.MethodPatch)
+	router.HandleFunc("/account", auth.Middleware(newAccount.AccountInfoHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/account", auth.Middleware(newAccount.UpdateHandler)).Methods(http.MethodPatch)
 
-	router.HandleFunc("/pay", auth.Middleware(paymentDB.PayHandler)).Methods(http.MethodPost)
-	
-	router.HandleFunc("/admin/sellers", auth.Middleware(adminDB.SellerListHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/admin/unpayouts", auth.Middleware(adminDB.FetchUnPayoutHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/pay", auth.Middleware(newPayment.PayHandler)).Methods(http.MethodPost)
 
-	router.HandleFunc("/order", auth.Middleware(orderDB.ListOrdersHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/admin/sellers", auth.Middleware(newAdmin.SellerListHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/admin/unpayouts", auth.Middleware(newAdmin.FetchUnPayoutHandler)).Methods(http.MethodGet)
+
+	router.HandleFunc("/order", auth.Middleware(newOrder.ListOrdersHandler)).Methods(http.MethodGet)
 
 	// Start the HTTP server on port 8080
 	fmt.Println("Starting server on :8080...")
